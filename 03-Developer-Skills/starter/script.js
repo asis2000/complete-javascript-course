188e4ea22733462784adeca086b7f6b5cb8de5a3
asis2000 <asissharma2000@gmail.com>
2023-03-26 21:27:05 +0530

    Update script.js

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,14 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
-
+"use strict";
+const data1 = [17, 21, 23];
+const data2 = [12, 5, -5, 0, 4];
+let printForecast = (arr) => {
+  let str = "";
+  let i = 0;
+  arr.forEach((element) => {
+    str += element + "*C" + "in" + (i + 1) + "days...";
+    i++;
+  });
+  console.log("..." + str);
+};
+printForecast(data1);
